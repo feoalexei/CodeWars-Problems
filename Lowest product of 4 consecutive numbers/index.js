a390c3528299e3c70adf31b2b4aa36e575689476
feoalexei <feoalexei@ukr.net>
2020-07-08 12:25:49 +0300

    6 kyu

---
+++ Lowest product of 4 consecutive numbers/index.js
@@ -0,0 +1,26 @@
+// 4 //////////  Lowest product of 4 consecutive numbers //////////
+
+function lowestProduct(input) { 
+  const allProducts = [];
+  let product = 0;
+  for (let i = 0; i < input.length; i++) {
+    const four = [...input].slice(i,i+4);
+    if (four.length === 4) 
+      product = four.reduce((a,c) => a*c);
+      allProducts.push(product);
+    }  
+   return input.length >= 4 ? Math.min(...allProducts) : "Number is too small"
+}
+
+
+console.log(lowestProduct("2345611117899"));
+
+
+
+
+
+
+
+
+
+
---
+++ Lowest product of 4 consecutive numbers/readme.md
@@ -0,0 +1,12 @@
+6 kyu
+
+Lowest product of 4 consecutive numbers
+
+Create a function that returns the lowest product of 4 consecutive digits in a number given as a string.
+
+This should only work if the number has 4 digits or more. If not, return "Number is too small".
+
+Example
+lowestProduct("123456789")--> 24 (1x2x3x4)
+lowestProduct("35") --> "Number is too small"
+
