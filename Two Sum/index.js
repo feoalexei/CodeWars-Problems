d77311b22e4d0d5931e0635d2a5b7552b2063500
feoalexei <feoalexei@ukr.net>
2020-07-10 09:03:46 +0300

    6 kyu

---
+++ Two Sum/index.js
@@ -0,0 +1,25 @@
+// 6 ////////// Two Sum //////////
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+
+console.log(twoSum([1234,5678,9012], 14690));
+
+
+
+
+
+
+
+
+
+
+
