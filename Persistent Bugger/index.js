e3ad99ff656fb82609d7a9b880cecb3f1f1740b8
feoalexei <feoalexei@ukr.net>
2020-07-15 12:43:44 +0300

    6 kyu

---
+++ Persistent Bugger/index.js
@@ -0,0 +1,38 @@
+// 11 ////////// Persistent Bugger //////////
+
+function persistence(num) {
+  let counter = 0;
+
+  num = num.toString();
+
+  while (num.length > 1) {
+    num = [...num]
+      .map(e => parseInt(e))
+      .reduce((a,b) => a * b)
+      .toString();
+
+      counter++;
+  }  
+  return counter;
+} 
+
+
+console.log(persistence(999));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
