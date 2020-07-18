13a02ef9a7307ebcee4d603d63964ffea57e0b41
feoalexei <feoalexei@ukr.net>
2020-07-18 11:14:58 +0300

    5 kyu

---
+++ Sum of Pairs/index.js
@@ -0,0 +1,34 @@
+// 14 ////////// Sum of Pairs //////////
+
+const sum_pairs = function(ints, s) {
+    const appearSet = new Set();
+    for (let int of ints) {
+        if (appearSet.has(s - int)) return [s - int, int];
+        appearSet.add(int);        
+    }
+}
+
+console.log(sum_pairs([10, 5, 2, 3, 7, 5],         10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
