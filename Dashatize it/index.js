751e80ae6610bc9d9ea68f412352b736fd35754a
feoalexei <feoalexei@ukr.net>
2020-07-20 11:21:45 +0300

    6 kyu

---
+++ Dashatize it/index.js
@@ -0,0 +1,13 @@
+// 17 ////////// Dashatize it //////////
+
+function dashatize(num) {
+    return num.toString()
+        .replace(/([13579])/g, "-$1-")
+        .replace(/-+/g, "-")
+        .replace(/^-|-$/g, "")
+}
+        
+
+
+console.log(dashatize(6815));
+
